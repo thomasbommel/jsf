88abd8c75ada24cf1936c54363a894e4fb536af7
Koella1337 <endtmayer.alexander@gmx.at>
2018-06-12 12:06:57 +0200

    CG: Basic Setup for SceneGraph & Canvas.

--- cg_lab_2018-master/00_empty/main.js
+++ cg_lab_2018-master/00_empty/main.js
@@ -1,32 +1,8 @@
 //the OpenGL context
-var gl = null,
-    program = null;
+var gl = null;
 
-/**
- * initializes OpenGL context, compile shader, and load buffers
- */
-function init(resources) {
-  //create a GL context
-  gl = createContext(400 /*width*/, 400 /*height*/);
-
-  //compile and link shader program
-  program = createProgram(gl, resources.vs, resources.fs);
-}
-
-/**
- * render one frame
- */
-function render() {
-
-  gl.clearColor(0.9, 0.9, 0.9, 1.0);
-  //clear the buffer
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-  //TODO
-
-  //request another call as soon as possible
-  //requestAnimationFrame(render);
-}
+//scene graph nodes
+var root = null;
 
 //load the shader resources using a utility function
 loadResources({
@@ -38,3 +14,48 @@ loadResources({
   //render one frame
   render();
 });
+
+/**
+ * initializes OpenGL context, creates SceneGraph and loads buffers
+ */
+function init(resources) {
+  //create a GL context
+  gl = createContext(400 /*width*/, 400 /*height*/);
+
+  //create scenegraph
+  root = createSceneGraph(gl, resources);
+}
+
+/**
+ * creates the initial SceneGraph and returns it's root node
+ */
+function createSceneGraph(gl, resources) {
+  //compile and link shader program and create root node of SceneGraph with it
+  const root = new ShaderSGNode(createProgram(gl, resources.vs, resources.fs));
+
+
+
+  return root;
+}
+
+/**
+ * renders a single frame
+ */
+function render(/*float*/ timeInMilliseconds){
+  //keep window at maximum size possible
+  checkForWindowResize(gl);
+
+  //display time with 0 comma-places for debugging - remove later
+  displayText(parseFloat(timeInMilliseconds).toFixed(0));
+
+  //setup viewport
+  gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+  gl.clearColor(0.9, 0.9, 0.9, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  //TODO
+
+
+  //request another call as soon as possible (for animation)
+  requestAnimationFrame(render);
+}
