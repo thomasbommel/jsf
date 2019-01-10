a21e33bb6e792122e44d1dcf39965d1e12517340
Gerald <Gerald@GeheimnisvollerGeist.lan>
2019-01-10 15:21:52 +0100

    Changes to Position Class(mainly getters, setters and vector operations)

--- Software_Engineering/src/main/java/factory/shared/Position.java
+++ Software_Engineering/src/main/java/factory/shared/Position.java
@@ -5,19 +5,62 @@ package factory.shared;
  */
 public class Position {
 
-	public void setPositionX(double xPos) {
-		//TODO
+	Double xPos, yPos;
+	
+	public Position(Double xPos, Double yPos)
+	{
+		this.xPos = xPos;
+		this.yPos = yPos;
+	}
+	
+	public void setPositionX(Double xPos) {
+		this.xPos = xPos;
 	}
 
-	public void setPositionY(double yPos) {
-		//TODO
+	public void setPositionY(Double yPos) {
+		this.yPos = yPos;
 	}
 
-	public double getPositionX() {
-		return 0;	//TODO
+	public Double getPositionX() {
+		return xPos;
 	}
 
-	public double getPositionY() {
-		return 0;	//TODO
+	public Double getPositionY() {
+		return yPos;
+	}
+	
+	public Position add(Position pos2)
+	{
+		return new Position(xPos+pos2.getPositionX(), yPos+pos2.getPositionY());
+	}
+	
+	public Position subtract(Position pos2)
+	{
+		return new Position(xPos-pos2.getPositionX(), yPos-pos2.getPositionY());
+	}
+	
+	public Position multiply(Double mult)
+	{
+		return new Position(xPos*mult, yPos*mult);
+	}
+	
+	public Position divide(Double div)
+	{
+		return new Position(xPos/div, yPos/div);
+	}
+	
+	public double length()
+	{
+		return Math.sqrt(xPos*xPos+yPos*yPos);
+	}
+	
+	public boolean equals(Position pos2)
+	{
+		return xPos == pos2.getPositionX() && yPos == pos2.getPositionY();
+	}
+	
+	public String toString()
+	{
+		return xPos + ", " + yPos;
 	}
 }
