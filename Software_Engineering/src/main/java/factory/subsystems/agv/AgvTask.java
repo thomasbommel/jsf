c5c3ea36670cb7b3577a3da28ed827ef085887f3
Gerald <Gerald@GeheimnisvollerGeist.lan>
2019-01-10 15:41:18 +0100

    actually implemented the AgvTask Class

--- Software_Engineering/src/main/java/factory/subsystems/agv/AgvTask.java
+++ Software_Engineering/src/main/java/factory/subsystems/agv/AgvTask.java
@@ -1,9 +1,31 @@
 package factory.subsystems.agv;
 
+import factory.shared.Position;
+import factory.shared.ResourceBox;
 import factory.shared.Task;
 
 public class AgvTask extends Task {
 
+	private final ResourceBox box;
+	private final Position pickup, dropoff;
 	
+	public AgvTask(int id, ResourceBox box, Position pickup, Position dropoff) {
+		super();
+		this.box = box;
+		this.pickup = pickup;
+		this.dropoff = dropoff;
+	}
 
+	public ResourceBox getBox() {
+		return box;
+	}
+
+	public Position getPickup() {
+		return pickup;
+	}
+
+	public Position getDropoff() {
+		return dropoff;
+	}
+	
 }
